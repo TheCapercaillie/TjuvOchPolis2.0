57d04814bf60b2e718ea57772a6132f402047204
TheCapercaillie <jonathan.tjader@gmail.com>
2024-10-24 10:12:50 +0200

    Added the class "Map" with some code (forgot to commit erlier changes)

---
+++ TjuvOchPolis2.0/Map.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis2._0
+{
+    internal class Map
+    {
+        private int width;
+        private int height;
+        private char[,] grid;
+
+        public Map(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            grid = new char[height, width];
+            
+        }
+    }
+}
